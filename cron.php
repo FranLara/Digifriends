f6d75eff8a54f2b51e2100e79641359ee62509b6
Fran Lara <francisco.lara.casalilla@gmail.com>
2016-08-02 12:30:50 +0200

    Added cron function

---
+++ cron.php
@@ -0,0 +1,12 @@
+<?php
+
+define("EXPIRED_TIME" , time() - (60*60*24*30));
+//define("EXPIRED_TIME" , time());
+define("QUERY_DELETE_USERS_SEARCHS", "DELETE FROM user_number_request WHERE date_request < " . EXPIRED_TIME);
+
+function cron(){
+	
+	$connection = new MySQL_Connection();
+	$deleteRecords = $connection->executeQuery(QUERY_DELETE_USERS_SEARCHS);
+	
+}
